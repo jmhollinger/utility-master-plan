6042802c86b7ef9cae0759493311d459487d2216
Jonathan Hollinger <jhollinger@lexingtonky.gov>
2016-09-02 11:14:02 -0400

    Update

--- public/js/map.js
+++ public/js/map.js
@@ -1,171 +1,199 @@
 function initialize(){
 
-  //For address searching.
   var markers = [];
-
-  //Set your basic options.
+  
   var mapOptions = {
-    tilt: 0,
-    zoom: 12,
-    center: {lat: 38.047594, lng: -84.496428},
     overviewMapControl:true,
     rotateControl:true,
     scaleControl:true,
     mapTypeControl: true,
+    mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR, position:google.maps.ControlPosition.TOP_CENTER},
     zoomControl: true,
+    zoomControlOptions: {style: google.maps.ZoomControlStyle.DEFAULT}
     };
 
-  //Fill up the map-canvas.
-  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+ var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-  //Drawing Library Config.
-  var drawingManager = new google.maps.drawing.DrawingManager({
-    drawingControl: true,
-    drawingControlOptions: {
-      position: google.maps.ControlPosition.TOP_CENTER,
-      drawingModes: [
-        google.maps.drawing.OverlayType.POLYLINE
-      ]},
-    polylineOptions: {
-      clickable: true
-    }  
+var infowindow = new google.maps.InfoWindow();
 
-    });
-  drawingManager.setMap(map);
+  var usaStyle = {
+    strokeWeight: 0,
+    fillColor: '#555',
+    fillOpacity: '0.4'
+    };
 
-  //Drawing library listener for completed lines
-  google.maps.event.addListener(drawingManager, 'polylinecomplete', function(line) {
+ var purchased = {
+    strokeColor: '#16a085',
+    strokeWeight: 2,
+    fillColor: '#16a085',
+    fillOpacity: '0.5'
+    };
 
-    drawingManager.setOptions({
-      drawingControl: true,
-      drawingControlOptions: {
-      position: google.maps.ControlPosition.TOP_CENTER,
-      drawingModes: [
-      ]}
-    });
+  var donated = {
+    strokeColor: '#2980b9',
+    strokeWeight: 2,
+    fillColor: '#2980b9',
+    fillOpacity: '0.5'
+    };
 
-    drawingManager.setDrawingMode(null)
+   var underContract = {
+    strokeColor: '#2c3e50',
+    strokeWeight: 2,
+    fillColor: '#2c3e50',
+    fillOpacity: '0.5'
+    };
 
-    var points = []
-    var points_obj = {}
-    var p_array = line.getPath().getArray()
-    $.each(p_array, function( index, value ) {
-      var temp = []
-      temp.push(value.lng(), value.lat())
-      points.push(temp)
-      return points
-    });
+  var pdr = new google.maps.Data()
+  var usa = new google.maps.Data()
+
+  pdr.loadGeoJson('data/pdr.geojson')
+  usa.loadGeoJson('data/usa.geojson')
   
-    points_obj = 
-    {
-        "type": "LineString",
-        "coordinates": points
+  pdr.setStyle(function(feature){
+  var s = feature.getProperty('Status')
+  var pt = feature.getProperty('FundingTyp')
+
+  if (s === 'Closed' && pt === 'Purchase'){return purchased}
+  else if (s === 'Closed' && pt === 'Donation') {return donated}
+  else if (s === 'Under Contract'){return underContract}	
+  else {return ''}   
+  }
+  );
+
+  usa.setStyle(usaStyle);
+
+  pdr.setMap(map)
+  usa.setMap(map)
+  
+
+ pdr.addListener('click', function(event) {
+ var s = event.feature.getProperty('Status')
+ var pt = event.feature.getProperty('FundingTyp')
+ var content = ''
+ 
+ var purchaseInfo = '<h4 class="green-text">' + event.feature.getProperty('Address')  + '</h4>'+
+    '<h5 class="gray-text">Farm Information</h5>'+
+    '<p class="item"><strong>Application No: </strong>' + event.feature.getProperty('APPNUM') + '</p>' +
+    '<p class="item"><strong>Acreage: </strong>' + event.feature.getProperty('Acreage') + '</p>' +
+    '<p class="item"><strong>Farm Status: </strong>' + event.feature.getProperty('Status') + '</p>' +
+    '<p class="item"><strong>Acquisition Type: </strong>' + event.feature.getProperty('FundingTyp') + '</p>' +
+    
+    '<h5 class="gray-text">Purchase Information</h5>'+
+    '<p class="item"><strong>Fiscal Year: </strong>' + event.feature.getProperty('FiscalYear') + '</p>' +
+    '<p class="item"><strong>Closing Date: </strong>' + dateFormat(event.feature.getProperty('DateClosed')) + '</p>' +
+    '<p class="item"><strong>Total Easement Cost: </strong>' + numeral(event.feature.getProperty('TotalPaid')).format('$0,0.00') + '</p>' +
+    '<p class="item"><strong>Easement Cost Per Acre: </strong>' + numeral(event.feature.getProperty('PerAcre')).format('$0,0.00') + '</p>' +
+    '<p class="item"><strong>Entity Paid: </strong>' + event.feature.getProperty('EntityPaid') + '</p>'
+ 	
+ var donationInfo = '<h4 class="green-text">' + event.feature.getProperty('Address')  + '</h4>'+
+    '<h5 class="gray-text">Farm Information</h5>'+
+    '<p class="item"><strong>Application No: </strong>' + event.feature.getProperty('APPNUM') + '</p>' +
+    '<p class="item"><strong>Acreage: </strong>' + event.feature.getProperty('Acreage') + '</p>' +
+    '<p class="item"><strong>Farm Status: </strong>' + event.feature.getProperty('Status') + '</p>' +
+    '<p class="item"><strong>Acquisition Type: </strong>' + event.feature.getProperty('FundingTyp') + '</p>' +
+    
+    '<h5 class="gray-text">Donation Information</h5>'+
+    '<p class="item"><strong>Fiscal Year: </strong>' + event.feature.getProperty('FiscalYear') + '</p>' +
+    '<p class="item"><strong>Closing Date: </strong>' + dateFormat(event.feature.getProperty('DateClosed')) + '</p>' +
+    '<p class="item"><strong>Donating Entity: </strong>' + event.feature.getProperty('EntityPaid') + '</p>'
+ 
+ var underContractInfo = '<h4 class="green-text">' + event.feature.getProperty('Address')  + '</h4>'+
+    '<h5 class="gray-text">Farm Information</h5>'+
+    '<p class="item"><strong>Application No: </strong>' + event.feature.getProperty('APPNUM') + '</p>' +
+    '<p class="item"><strong>Acreage: </strong>' + event.feature.getProperty('Acreage') + '</p>' +
+    '<p class="item"><strong>Farm Status: </strong>' + event.feature.getProperty('Status') + '</p>' +
+    '<p class="item"><strong>Acquisition Type: </strong>' + event.feature.getProperty('FundingTyp') + '</p>'
+
+  if (s === 'Closed' && pt === 'Purchase'){content = purchaseInfo}
+  else if (s === 'Closed' && pt === 'Donation') {content = donationInfo}
+  else if (s === 'Under Contract'){content = underContractInfo}	
+  else {content = 'No information available.'}   
+
+    infowindow.setContent(content)
+
+    var anchor = new google.maps.MVCObject();
+        anchor.setValues({ //position of the point
+            position: event.latLng,
+            anchorPoint: new google.maps.Point(0, 0)
+        });
+
+        infowindow.open(map, anchor);
+  });
+   
+
+  var defaultBounds = new google.maps.LatLngBounds(
+      new google.maps.LatLng(37.921971, -84.663139),
+      new google.maps.LatLng(38.155595, -84.334923)
+      );
+  
+  map.fitBounds(defaultBounds);
+
+  // Create the search box and link it to the UI element.
+  var input = /** @type {HTMLInputElement} */(
+      document.getElementById('pac-input'));
+
+  var searchBox = new google.maps.places.SearchBox(
+    /** @type {HTMLInputElement} */(input));
+
+  // [START region_getplaces]
+  // Listen for the event fired when the user selects an item from the
+  // pick list. Retrieve the matching places for that item.
+  google.maps.event.addListener(searchBox, 'places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+    for (var i = 0, marker; marker = markers[i]; i++) {
+      marker.setMap(null);
     }
 
+    // For each place, get the icon, place name, and location.
+    markers = [];
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0, place; place = places[i]; i++) {
+      var image = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      var marker = new google.maps.Marker({
+        map: map,
+        icon: image,
+        title: place.name,
+        position: place.geometry.location
+      });
+
+      markers.push(marker);
+
+      bounds.extend(place.geometry.location);
+    }
     
-    $('#coordinates').val(JSON.stringify(points_obj))
-    
-
-    $('#clear-map').click(function(){
-    line.setMap(null)
-
-    drawingManager.setOptions({
-      drawingControl: true,
-      drawingControlOptions: {
-      position: google.maps.ControlPosition.TOP_CENTER,
-      drawingModes: [
-      google.maps.drawing.OverlayType.POLYLINE
-      ]}
-    })
-  
-    $('#coordinates').val('')
-
-  })
-    
-
+    map.fitBounds(bounds);
+    map.setZoom(15)
   });
+  // [END region_getplaces]
 
 
-//Listen for street selection and zoom map.
-$('#street').bind('typeahead:selected', function() {
-  var coor = getLatLng($(this).val())
-  var point  = new google.maps.LatLng(coor[0], coor[1])
-  if(coor[0]){
-    map.setCenter(point)  
-    map.setZoom(coor[2])
-  }
-  else {}
-    })
 
-
-  // Create the search box and link it to the HTML element.
-    var input = /** @type {HTMLInputElement} */(
-        document.getElementById('pac-input'));
-
-    var searchBox = new google.maps.places.SearchBox(
-      /** @type {HTMLInputElement} */(input));
-
-    // Listen for search box changes.
-    google.maps.event.addListener(searchBox, 'places_changed', function() {
-      var places = searchBox.getPlaces();
-
-      if (places.length == 0) {
-        return;
-      }
-      for (var i = 0, marker; marker = markers[i]; i++) {
-        marker.setMap(null);
-      }
-
-      // For each place, get the icon, place name, and location.
-      markers = [];
-      var bounds = new google.maps.LatLngBounds();
-      for (var i = 0, place; place = places[i]; i++) {
-        var image = {
-          url: place.icon,
-          size: new google.maps.Size(71, 71),
-          origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(17, 34),
-          scaledSize: new google.maps.Size(25, 25)
-        };
-
-        // Create a marker for each place.
-        var marker = new google.maps.Marker({
-          map: map,
-          icon: image,
-          title: place.name,
-          position: place.geometry.location
-        });
-
-
-        markers.push(marker);
-
-        bounds.extend(place.geometry.location);
-      }
-      
-      map.fitBounds(bounds);
-      //Set zoom level for when an address is selected.
-      map.setZoom(16)
-        });
-    
-    // Bias the SearchBox results towards places that are within the bounds of the
-    // current map's viewport.
-    google.maps.event.addListener(map, 'bounds_changed', function() {
-      var bounds = map.getBounds();
-      searchBox.setBounds(bounds);
-    });
-
+  // Bias the SearchBox results towards places that are within the bounds of the
+  // current map's viewport.
+  google.maps.event.addListener(map, 'bounds_changed', function() {
+    var bounds = map.getBounds();
+    searchBox.setBounds(bounds);
+  });
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
-//Function to get coordinates of selected street
-function getLatLng (streetname) {
-var latlng = []
-  $.each(streets.streets, function( index, value ) {
-    if (value.name === streetname){
-      latlng = [value.lat, value.lng, 16]
-      return false
-    }
-  })
-return latlng
+function dateFormat(input){
+	if(input){
+var parts = input.split('/')
+return parts[1] + '/' + parts[2] + '/' + parts[0]
+}
+else{}
 }
