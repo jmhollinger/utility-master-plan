f840b58a00ebd4b54eaf553cc5904d1b34b4193f
Jonathan Hollinger <jmholl5@gmail.com>
2017-04-21 14:37:01 -0400

    Update

--- index.js
+++ index.js
@@ -210,8 +210,6 @@ app.get('/list', stormpath.groupsRequired(['Utilities', 'Admins'], false), funct
 
                   formattedData.push(item)
                 }
-
-                console.log(formattedData)
                 res.render('list', 
                   {
                     data : formattedData });           
