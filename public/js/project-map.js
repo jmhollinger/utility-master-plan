93b98c30f958c5cd08026dc71f13fb87aa806a0a
Jonathan Hollinger <jmholl5@gmail.com>
2017-04-26 14:47:36 -0400

    Update

--- public/js/project-map.js
+++ public/js/project-map.js
@@ -19,65 +19,6 @@ function initialize() {
 
     var map = new google.maps.Map(document.getElementById('project-map'), mapOptions);
 
-    var infowindow = new google.maps.InfoWindow();
-
-
-    var blue = {
-        strokeColor: '#2980b9',
-        strokeWeight: 6,
-        fillColor: '#2980b9',
-        fillOpacity: '0.5'
-    };
-
-    var red = {
-        strokeColor: '#e74c3c',
-        strokeWeight: 6,
-        fillColor: '#e74c3c',
-        fillOpacity: '0.5'
-    };
-
-    var plan = new google.maps.Data()
-
-
-    plan.loadGeoJson('https://lfucg-master-plan.herokuapp.com/api/v1/projectmap')
-
-    plan.setStyle(function(feature) {
-        if (feature.getProperty('StreetCut') === 'Yes') {
-            return red
-        } else {
-            return blue
-        }
-    });
-
-    plan.setMap(map)
-
-    plan.addListener('click', function(event) {
-
-        var content =
-            '<h4>' + event.feature.getProperty("Utility") + '</h4>' +
-            '<p>' + event.feature.getProperty("Street") + ' (' + event.feature.getProperty("Intersection1") + ' - ' + event.feature.getProperty("Intersection2") + ')</p>' +
-            '<strong>Name: </strong>' + event.feature.getProperty("Name") + '<br/>' +
-            '<strong>Description: </strong>' + event.feature.getProperty("Description") + '<br/>' +
-            '<strong>Impacts: </strong>' + event.feature.getProperty("Impacts") + '<br/>' +
-            '<strong>Start Date: </strong>' + event.feature.getProperty("StartDate") + '<br/>' +
-            '<strong>End Date: </strong>' + event.feature.getProperty("EndDate") + '<br/>' +
-            '<strong>Days in ROW: </strong>' + event.feature.getProperty("DaysinROW") + '<br/>' +
-            '<strong>Type: </strong>' + event.feature.getProperty("Type") + '<br/>' +
-            '<strong>Street Cut: </strong>' + event.feature.getProperty("StreetCut") +
-            '<a href=\"' + event.feature.getProperty("ProjectUrl") + '\" class=\"btn btn-default\"'
-
-        infowindow.setContent(content)
-
-        var anchor = new google.maps.MVCObject();
-        anchor.setValues({ //position of the point
-            position: event.latLng,
-            anchorPoint: new google.maps.Point(0, 0)
-        });
-
-        infowindow.open(map, anchor);
-    });
-
-
     var defaultBounds = new google.maps.LatLngBounds(
         new google.maps.LatLng(37.921971, -84.663139),
         new google.maps.LatLng(38.155595, -84.334923)
@@ -146,10 +87,3 @@ function initialize() {
     });
 }
     google.maps.event.addDomListener(window, 'load', initialize);
-
-    function dateFormat(input) {
-        if (input) {
-            var parts = input.split('/');
-            return parts[1] + '/' + parts[2] + '/' + parts[0];
-        } else {}
-    }
